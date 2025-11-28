f655c9e2e820d473e25e7e74d985361d3902bf90
maimoona-stack <245123737065@mvsrec.edu.in>
2025-11-28 20:37:32 +0530

    home.php

--- booksite/home.php
+++ booksite/home.php
@@ -6,14 +6,14 @@
 
 <frameset rows="22%,78%" border="0">
 
-    <!-- TOP SECTION -->
+
     <frame src="top.php" name="top" scrolling="no" noresize>
 
-    <!-- LEFT + RIGHT SECTION -->
+ 
     <frameset cols="20%,80%" border="0">
 
         <frame src="left.php" name="left" noresize>
-        <frame src="mvsr_info.php" name="right">  <!-- DEFAULT PAGE -->
+        <frame src="mvsr_info.php" name="right"> 
 
     </frameset>
 
